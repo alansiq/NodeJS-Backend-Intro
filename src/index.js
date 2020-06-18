a581289971a5f1df29adaae7c1b04cdbdc601ec1
Alan Siqueira <alan.siqueira@outlook.com>
2020-06-17 23:17:25 -0300

    Added routes filtering and Middleware to log routes usage

--- src/index.js
+++ src/index.js
@@ -1,31 +1,94 @@
 const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
 
 
 const app = express();
+app.use(express.json());
+
+const projects = [];
+
+function logRequests(request, response, next) {
+    const {method, url } = request;
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(logLabel);
+
+    return next();
+}
+
+function validateProjectId(request, response, next) {
+    // validar se o id no put ou delete é um id válido.
+    const { id } =  request.params;
+
+    if ( !isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid project ID.'});
+    }
+
+    return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId)
 
 app.get('/projects', (request, response) => {
-    const {title, owner} = request.query;
-    // ?title=Alan&?owner=Alan
-    console.log(title);
-    console.log(owner);
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+    const {title} = request.query;
+    // ?title=Alan&owner=Alan
+    const results = title 
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
-    return response.json({response: 'Resposta post'})
+
+    const { title, owner } = request.body;
+
+    const project = { id: uuid() ,title, owner};
+
+    projects.push(project);
+
+    return response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
-    return response.json({response: 'Resposta put'})
+
+    const { id } = request.params;
+    const { title, owner } = request.body;
+    
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(404).json({error: 'Project not found'})
+        
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+
+    projects[projectIndex] = project;
+
+    return response.json(project)
+    
 })
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json({response: 'Resposta delete'})
+    const { id } = request.params;
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(404).json({error: 'Project not found'})
+        
+    }
+
+    projects.splice(projectIndex, 1);
+
+
+    return response.status(204).send()
 })
 
 
